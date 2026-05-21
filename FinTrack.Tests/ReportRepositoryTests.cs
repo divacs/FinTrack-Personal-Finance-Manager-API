788ceea15221b88b32e3980d70be6b698d90df46
divacs <sonja.divac@yahoo.com>
2026-05-21 21:26:00 +0200

    fix: stabilize report repository test seed

--- FinTrack.Tests/ReportRepositoryTests.cs
+++ FinTrack.Tests/ReportRepositoryTests.cs
@@ -27,16 +27,21 @@ public class ReportRepositoryTests : IDisposable
         _context.Database.EnsureCreated();
 
         // seed user and categories, transactions
-        var user = new ApplicationUser { Id = "u1", UserName = "u1", Email = "u1@x.com" };
+        var user = new ApplicationUser { Id = "u1", UserName = "u1", Email = "u1@x.com", FirstName = "Test", LastName = "User" };
         _context.Users.Add(user);
 
-        var catIncome = new Category { Id = 1, Name = "Salary" };
-        var catExpense = new Category { Id = 2, Name = "Groceries" };
-        _context.Categories.AddRange(catIncome, catExpense);
+        _context.BankAccounts.Add(new BankAccount
+        {
+            Id = 2,
+            BankName = "Test Bank",
+            AccountNumber = "TEST-ACCOUNT",
+            Balance = 0,
+            UserId = user.Id
+        });
 
         _context.Transactions.AddRange(
-            new Transaction { Id = 1, BankAccountId = 0, Amount = 1000m, Date = new DateTime(2025, 11, 5), CategoryId = 1, Type = "Income" },
-            new Transaction { Id = 2, BankAccountId = 0, Amount = 200m, Date = new DateTime(2025, 11, 10), CategoryId = 2, Type = "Expense" }
+            new Transaction { Id = 3, BankAccountId = 2, Amount = 1000m, Date = new DateTime(2025, 11, 5), CategoryId = 5, Type = "Income" },
+            new Transaction { Id = 4, BankAccountId = 2, Amount = 200m, Date = new DateTime(2025, 11, 10), CategoryId = 1, Type = "Expense" }
         );
 
         _context.SaveChanges();
